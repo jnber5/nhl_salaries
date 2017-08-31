c8480d65725a93585789bbb6fafa53646c46ace0
Jean-Nicholas Berube <jean.nicholas.berube@gmail.com>
2017-08-31 14:48:49 -0400

    Duree

--- admin.js
+++ admin.js
@@ -3,57 +3,60 @@ var serviceAccount = require('../firebase/lhplge-b145e-firebase-adminsdk-n79o5-1
 var joueurs = require('./joueurs.json');
 var joueursAutonomes = require('./joueurs-autonomes.json');
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: 'https://lhplge-b145e.firebaseio.com'
-});
-
-const db = admin.database();
-
-const joueursRef = db.ref('joueurs');
-
-for (let joueur of joueurs) {
-  let id = joueur.prenom + joueur.nom;
-  id = id
-    .replace(' ')
-    .replace('.', '')
-    .replace('.', '')
-    .replace('-', '')
-    .toLowerCase();
-
-  joueursRef.child(id).set({
-    nom: joueur.nom,
-    prenom: joueur.prenom,
-    equipe: toTitleCase(joueur.equipe.toLowerCase()),
-    position: joueur.position,
-    age: parseInt(joueur.age),
-    caphit: parseInt(joueur.caphit),
-    capFriendlyUrl: joueur.capFriendlyUrl,
-    franchise: '',
-    estAutonome: true
+function main() {
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: 'https://lhplge-b145e.firebaseio.com'
   });
-}
 
-for (let joueur of joueursAutonomes) {
-  let id = joueur.prenom + joueur.nom;
-  id = id
-    .replace(' ')
-    .replace('.', '')
-    .replace('.', '')
-    .replace('-', '')
-    .toLowerCase();
+  const db = admin.database();
 
-  joueursRef.child(id).set({
-    nom: joueur.nom,
-    prenom: joueur.prenom,
-    equipe: toTitleCase(joueur.equipe.toLowerCase()),
-    position: joueur.position,
-    age: parseInt(joueur.age),
-    caphit: parseInt(joueur.caphit),
-    capFriendlyUrl: joueur.capFriendlyUrl,
-    franchise: '',
-    estAutonome: true
-  });
+  const joueursRef = db.ref('joueurs');
+
+  for (let joueur of joueurs) {
+    let id = joueur.prenom + joueur.nom;
+    id = id
+      .replace(' ')
+      .replace('.', '')
+      .replace('.', '')
+      .replace('-', '')
+      .toLowerCase();
+
+    joueursRef.child(id).set({
+      nom: joueur.nom,
+      prenom: joueur.prenom,
+      equipe: toTitleCase(joueur.equipe.toLowerCase()),
+      position: joueur.position,
+      age: parseInt(joueur.age),
+      caphit: parseInt(joueur.caphit),
+      duree: Math.floor(Math.random() * 3) + 1,
+      capFriendlyUrl: joueur.capFriendlyUrl,
+      franchise: '',
+      estAutonome: true
+    });
+  }
+
+  for (let joueur of joueursAutonomes) {
+    let id = joueur.prenom + joueur.nom;
+    id = id
+      .replace(' ')
+      .replace('.', '')
+      .replace('.', '')
+      .replace('-', '')
+      .toLowerCase();
+    joueursRef.child(id).set({
+      nom: joueur.nom,
+      prenom: joueur.prenom,
+      equipe: toTitleCase(joueur.equipe.toLowerCase()),
+      position: joueur.position,
+      age: parseInt(joueur.age),
+      caphit: parseInt(joueur.caphit),
+      duree: Math.floor(Math.random() * 3) + 1,
+      capFriendlyUrl: joueur.capFriendlyUrl,
+      franchise: '',
+      estAutonome: true
+    });
+  }
 }
 
 function toTitleCase(str) {
@@ -61,3 +64,5 @@ function toTitleCase(str) {
     return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
   });
 }
+
+main();
